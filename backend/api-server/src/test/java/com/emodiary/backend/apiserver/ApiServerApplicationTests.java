a7d60aa92e8ebac9134a60e035d370ec78a8f623
김형표 <jmn9798@naver.com>
2025-08-25 14:30:49 +0900

    chore: 백엔드 초기 세팅
    -HealthCheck 완료

---
+++ backend/api-server/src/test/java/com/emodiary/backend/apiserver/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emodiary.backend.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
